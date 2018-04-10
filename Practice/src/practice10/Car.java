76dd3f066794212967c110ba45fe53033fbb9c12
nakamura takuya <gunkan.co.jp@gmail.com>
2018-04-10 19:00:02 +0900

    no message

--- Practice/src/practice10/Car.java
+++ Practice/src/practice10/Car.java
@@ -1,5 +1,19 @@
 package practice10;
 
 public class Car {
+	int serialNo;
+	String color;
+	int gasoline;
+	public static int run() {
 
+		// ★ 0～15のランダムな値を取得して、変数numberに代入してください
+				int limitNumber = 15;
+
+				// ランダムで数字を生成するプログラムです。
+				// 下記の命令を実行すると変数randomに、0以上、変数limitNumber未満の数字がランダムで代入されます
+				int random = new java.util.Random().nextInt(limitNumber + 1);
+			    int number = random;
+			     int i = gasoline - number;
+			   return
+	}
 }
