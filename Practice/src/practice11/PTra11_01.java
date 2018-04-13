3062b971774c26a734931f9f7cf2df7a7d2dd06b
nakamura takuya <gunkan.co.jp@gmail.com>
2018-04-13 17:09:33 +0900

    PTra11完了。

--- Practice/src/practice11/PTra11_01.java
+++ Practice/src/practice11/PTra11_01.java
@@ -13,12 +13,9 @@ public class PTra11_01 {
 		 * 以下のプログラムではコンパイルエラーが起きてしまいます
 		 * ★ コンパイルが通るように修正してください(引数5つのコンストラクタを利用すること)
 		 */
-		Book book = new Book(null, null, 0, 0, null);
-		book.title = "スッキリわかるJava入門";
-		book.author = "中山清喬／国本大悟";
-		book.price = 2600;
-		book.pageSize = 657;
-		book.publication = "インプレス";
+		Book book = new Book("スッキリわかるJava入門","中山清喬／国本大悟", 2600,657,  "インプレス");
+
+
 
 		// 本の情報を出力します
 		System.out.println(book.dispBookInfo());
--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,21 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-		FileReaderClass.readBookDataFile();
+		Book[] bookList =  FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		String line = scanner.nextLine();
 
+		for (int i = 0; i < bookList.length; i++) {
+
+
+			if (bookList[i].title.indexOf(line)!= -1)  {
+				System.out.println(bookList[i].title);
+			}
+
+		}
 	}
 }
